2395436e4194d97ba522bcfbc86e818b8fd72563
Rodrigo Sanseverino de Anhaia <rodrigoanhaia2@hotmail.com>
2024-02-09 21:19:40 -0300

    First
    first

---
+++ PokéDex/Poke_DexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_DexApp.swift
+//  PokéDex
+//
+//  Created by Rodrigo Sanseverino de Anhaia on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Poke_DexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
